9b1d19d325bc5dba457a562366fbce4429a8e77a
Fong Yoong <fongyoong8@gmail.com>
2022-08-04 00:18:19 +0800

    VLC estimation with Kalman Filter

--- led_transmitter/led_transmitter.ino
+++ led_transmitter/led_transmitter.ino
@@ -1,7 +1,7 @@
 const int ledPin1 = 16;  // 16 corresponds to GPIO16 (red)
 const int ledPin2 = 17;  // 17 corresponds to GPIO17 (yellow)
-const int ledPin3 = 19;  // 1 corresponds to GPIO1 (blue)
-const int ledPin4 = 18;  // 3 corresponds to GPIO3 (black)
+const int ledPin3 = 19;  // 19 corresponds to GPIO1 (blue)
+const int ledPin4 = 18;  // 18 corresponds to GPIO3 (black)
 
 const int DutyCycle1 = 127; // 127/255 = 0.5,204/255=0.8
 
@@ -10,10 +10,10 @@ const double freq2 = 1250;
 const double freq3 = 1500;
 const double freq4 = 1750;
 
-const int ledChannel1 = 0;
-const int ledChannel2 = 1;
-const int ledChannel3 = 2;
-const int ledChannel4 = 3;
+const int ledChannel1 = 2;
+const int ledChannel2 = 4;
+const int ledChannel3 = 6;
+const int ledChannel4 = 8;
 
 const int resolution= 8;
  
@@ -29,20 +29,17 @@ void setup(){
   ledcAttachPin(ledPin2, ledChannel2);
   ledcAttachPin(ledPin3, ledChannel3);
   ledcAttachPin(ledPin4, ledChannel4);
-  
-  Serial.begin(115200);
-//  pinMode(16, OUTPUT);
-}
- 
-void loop(){
   ledcWrite(ledChannel1, DutyCycle1);
   ledcWrite(ledChannel2, DutyCycle1);
   ledcWrite(ledChannel3, DutyCycle1);
   ledcWrite(ledChannel4, DutyCycle1);
-  
-  Serial.println(123);
+  Serial.begin(115200);
+}
+ 
+void loop(){
+  Serial.println("Modulating lights...");
+  delay(1000);
 //  digitalWrite(16, HIGH);
-//  delay(500);
 //  digitalWrite(16, LOW);
 //  delay(500);
 }
--- trolley/src/Vlc/Vlc.h
+++ trolley/src/Vlc/Vlc.h
@@ -5,8 +5,9 @@
 #include "arduinoFFT.h"
 #include "../Utilities.h"
 #include "../Pins/Pins.h"
+#include "KalmanFilter1D.h"
 
-#define PHOTODIODE_INTERVAL 3000
+#define PHOTODIODE_INTERVAL 50
 #define FFT_SAMPLES 2048
 #define FFT_SAMPLING_FREQUENCY 4096.0
 // uint16_t Vlc::samples = 2048; //This value MUST ALWAYS be a power of 2
@@ -29,6 +30,10 @@ public:
     static unsigned int sampling_period_us;
     static unsigned long microseconds;
     
+    // These are the input and output vectors. Input vectors receive computed results from FFT
+    static double vReal[];
+    static double vImag[];
+
     //RSS for 4 LEDs
     static double Pr_1;
     static double Pr_2;
@@ -43,15 +48,20 @@ public:
     static double RX_x;
     static double RX_y;
 
-    // These are the input and output vectors. Input vectors receive computed results from FFT
-    static double vReal[];
-    static double vImag[];
+    // Kalman filter
+    static KalmanFilter1D rss1KalmanFilter;
+    static KalmanFilter1D rss2KalmanFilter;
+    static KalmanFilter1D rss3KalmanFilter;
+    static KalmanFilter1D rss4KalmanFilter;
+    // static KalmanFilter1D xKalmanFilter;
+    // static KalmanFilter1D yKalmanFilter;
 
     static void PrintVector(double *vData, uint16_t bufferSize, uint8_t scaleType);
     static void EstimateRSS(double *vData, uint16_t bufferSize, double *RSS1, double *RSS2, double *RSS3, double *RSS4);
+    static double RemoveRSSNoise(double RSS);
     static void EstimateDistance(double RSS1, double RSS2, double RSS3, double RSS4, double *D1, double *D2, double *D3, double *D4);
     static void EstimateCoordinate(double Dist_xyz1, double Dist_xyz2, double Dist_xyz3, double Dist_xyz4, double *Xe, double *Ye );
-
+    static void EstimateKalman();
 };
 
 #endif
